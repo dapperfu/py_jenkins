9f78df487d00e488c9945e81dd6ba3a35155e41d
Jed <jed-frey@users.noreply.github.com>
2017-10-03 02:33:42 -0400

    Updated files

--- setup.py
+++ setup.py
@@ -3,19 +3,17 @@
 # https://setuptools.readthedocs.io/en/latest/setuptools.html#new-and-changed-setup-keywords
 
 
-from setuptools import setup, find_packages
+from setuptools import setup
 import os
 
 requires = dict()
 for requirement in ["setup", "install", "tests"]:
     requires_txt = os.path.join(".requirements",
-    "{}.txt".format(requirement))
+                                "{}.txt".format(requirement))
     if os.path.exists(requires_txt):
         with open(requires_txt, "r") as f:
             require_pkgs = f.read().splitlines()
         requires[requirement] = require_pkgs
-
-print(requires)
 
 setup(name='py_jenkins',
       python_requires='>=2.7,!=3.0.*,!=3.1.*',
