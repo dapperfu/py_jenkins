fc1bd8adbc533e82dfff92c4babb6b975ad2ee51
Jed <jed-frey@users.noreply.github.com>
2017-10-03 10:44:06 -0400

    Adding more tests and configuring them

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
+# -*- coding: utf-8 -*-
 """Python Setup File."""
 
 # https://setuptools.readthedocs.io/en/latest/setuptools.html#new-and-changed-setup-keywords
 
 
-from setuptools import setup
 import os
 
-requires = dict()
+from setuptools import setup
+
+requires = {}
 for requirement in ["setup", "install", "tests"]:
-    requires_txt = os.path.join(".requirements",
-                                "{}.txt".format(requirement))
+    requires_txt = os.path.join(
+        ".requirements",
+        "{}.txt".format(requirement),
+    )
     if os.path.exists(requires_txt):
         with open(requires_txt, "r") as f:
             require_pkgs = f.read().splitlines()
         requires[requirement] = require_pkgs
 
-setup(name='py_jenkins',
-      python_requires='>=2.7,!=3.0.*,!=3.1.*',
-      setup_requires=requires["setup"],
-      install_requires=requires["install"],
-      tests_require=requires["tests"],
-      test_suite="pytest",
-      )
+setup(
+    name='py_jenkins',
+    python_requires='>=2.7,!=3.0.*,!=3.1.*',
+    setup_requires=requires["setup"],
+    install_requires=requires["install"],
+    tests_require=requires["tests"],
+    test_suite="pytest",
+)
--- tests/test_01.py
+++ tests/test_01.py
@@ -1,2 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Tests 1."""
+
+import random
+import time
+
+
 def test_hello():
+    """Hello."""
     print("World!")
+
+
+def test_slow():
+    """Artificial delayed test."""
+    time.sleep(random.randint(0, 10))
